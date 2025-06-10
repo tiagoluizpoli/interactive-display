5005aca667da2a8a797468742d7ee9130e38d416
Tiago Poli <tiagoluizpoli@gmail.com>
2025-06-10 02:01:12 -0300

    add animations

--- apps/frontend/src/presentation/output/layout-2.tsx
+++ apps/frontend/src/presentation/output/layout-2.tsx
@@ -1,21 +1,34 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { usePresentationConnection } from './presentation-connection';
 
 export const Layout2 = () => {
   const { currentSlide } = usePresentationConnection();
-
   const { displayEnabled, currentSlide: slide } = currentSlide;
 
-  if (!slide || !displayEnabled) {
-    return <div />;
-  }
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-end">
-      <div className="flex justify-center items-center w-full p-8">
-        <p className="text-5xl font-bold max-w-4xl text-center bg-[rgba(0,0,0,0.6)] text-white p-8 rounded-2xl">
-          {slide.text}
-        </p>
-      </div>
+    <div className="w-screen h-screen flex flex-col justify-end motion-preset-fade-lg">
+      <AnimatePresence mode="wait">
+        {slide && displayEnabled && (
+          <motion.div
+            key={`${slide.text}-${displayEnabled}`}
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            transition={{ duration: 0.3 }}
+            className="flex justify-center items-center w-full p-8"
+          >
+            <p className="text-5xl font-bold max-w-4xl text-center bg-[rgba(0,0,0,0.6)] text-white p-8 rounded-2xl">
+              {slide.text}
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 

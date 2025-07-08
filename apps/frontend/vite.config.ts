e8f836d96343fad4a6a00f90205fd9269e5a5524
Tiago Luiz Seuaciuc de Poli <tiagoluizpoli@gmail.com>
2025-07-08 01:32:12 -0300

    make front use port from environment

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,20 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    port: 5001,
-  },
-  preview: {
-    port: 5001,
-  },
-  resolve: {
-    alias: {
-      '@/src': path.resolve(process.cwd(), './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = env.PORT ? Number.parseInt(env.PORT) : 5001;
+
+  return {
+    plugins: [tailwindcss(), react()],
+    server: {
+      port: port,
     },
-  },
+    preview: {
+      port: port,
+    },
+    resolve: {
+      alias: {
+        '@/src': path.resolve(process.cwd(), './src'),
+      },
+    },
+  };
 });

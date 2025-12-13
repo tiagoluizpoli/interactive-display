f11f176b044e514f37c01a0c9d69446f14bce512
Tiago Poli <tiagoluizpoli@gmail.com>
2025-12-12 22:23:03 -0300

    adjust dockerization

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/**/*.ts'],
+  entry: ['src/**/*.ts', 'drizzle.config.ts'],
   clean: true,
   format: ['cjs'],
   ignoreWatch: ['src/db/migrations/**/*.sql'],
